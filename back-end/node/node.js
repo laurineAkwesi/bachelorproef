cd8101693f2e029ca1cd3f9af01fd93335345b2c
laurine akwesi <laurine.akwesi.mobuka@student.ehb.be>
2021-05-02 20:41:03 +0200

    html/node/mysql connection

---
+++ back-end/node/node.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const port = 63342;
+const assert = require('assert');
+
+const username = "laurineAkwesi";
+const password = "Laurine123";
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${username}:${password}@Struktr.u3qze.mongodb.net/bachelorproef?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+let collection;
+
+client.connect(err => {
+    collection = client.db("bachelorproef").collection("userinfo");
+    // perform actions on the collection object
+    console.log('We are connected bitches');
+    runProgram();
+});
+
+
+let runProgram = () => {
+    collection.insertOne({
+        "userId": Math.round(Math.random() * 100),
+        "username": "laurineAK",
+        "name": "laurine",
+        "lastname": "akwesi",
+        "email": "email"
+    }), function (err, result) {
+        assert.equal(err, null);
+        console.log(err)
+        console.log("inserted");
+        console.log(result)
+    }
+
+    app.post('/api/newUser', (req, res) => {
+        if (req.body.password != req.body.passwordCheck) {
+            console.log("password is not correct")
+        } else {
+            let username = req.body.username;
+            let name = req.body.name;
+            let lastname = req.body.lastname;
+            let password = req.body.password;
+            let email = req.body.email;
+
+                collection.insertOne({
+                    "userId": Math.round(Math.random() * 100),
+                    "username": username,
+                    "name": name,
+                    "lastname": lastname,
+                    "password": password,
+                    "email": email,
+                    "savedObjects": [],
+                    "ownTeam": []
+                }, function (err, result) {
+                    assert.equal(err, null);
+                    console.log(err)
+                    console.log("inserted");
+                    console.log(result)
+                })
+        }
+    })
+};
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Bitch!')
+});
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+});
+
---
+++ js/home.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    console.log("ready");
+    quote();
+    login();
+    newUser();
+
+    $('.stoornisDetail').hide();
+
+
+    $('.leesMeer').on("click", function () {
+        $('.stoornis').hide();
+        $('.stoornisDetail').show();
+    })
+});
+
+function quote() {
+    $.ajax({
+        url: `https://type.fit/api/quotes`,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (data) {
+        console.log(data);
+        let randomQuote = Math.floor((Math.random() * 100) + 1);
+        let quote = data[randomQuote];
+        console.log(quote.text);
+        $(`<text x="30%" y="35%" class="svgText">`).text(`${quote.text}`).appendTo('.svg');
+    })
+}
+
+function login() {
+    $('.loginFormulier').hide();
+    $('.login').on("click", function () {
+        $('.loginFormulier').show()
+        $('.profielInfo').hide();
+        $('.dagboek').hide();
+        $('.dagboekbutton').hide();
+    })
+}
+
+function newUser() {
+    $("form#toevoegenForm").submit(function () {
+        let newUser = {
+            username: $('#newUsername').val(),
+            name: $('#newName').val(),
+            lastname: $('#newLastName').val(),
+            email: $('#newEmail').val(),
+            password: $('#newPassword').val(),
+            passwordCheck: $('#newPasswordCheck').val()
+        }
+        console.log(newUser)
+        console.log("pressed");
+        $.ajax({
+            url: "http://127.0.0.1:63342/api/newUser",
+            type: "POST",
+            data: newUser,
+            success: function (data) {
+                console.log('added!')
+                //window.location.href = "login.html";
+            }
+        }).fail(function (err1, err2) {
+            console.log(err1)
+            console.log(err2)
+        })
+    });
+}
+
