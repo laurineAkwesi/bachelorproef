e2bc03a1252037b77095546f7d2227ba6f26ba79
laurine akwesi <laurine.akwesi.mobuka@student.ehb.be>
2021-05-19 22:49:53 +0200

    login

--- back-end/node/node.js
+++ back-end/node/node.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const bodyparser = require('body-parser')
 const port = 63342;
 const assert = require('assert');
+const ObjectID = require('mongodb').ObjectID;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,12 +21,17 @@ app.use(bodyparser.urlencoded({
     extended: false
 }));
 
+app.use(bodyparser.json());
+
 const username = "laurineAkwesi";
 const password = "Laurine123";
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${username}:${password}@Struktr.u3qze.mongodb.net/bachelorproef?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 let collection;
 
 client.connect(err => {
@@ -32,20 +43,46 @@ client.connect(err => {
 
 
 let runProgram = () => {
-    /*collection.insertOne({
-        "userId": Math.round(Math.random() * 100),
-        "username": "laurineAK",
-        "name": "laurine",
-        "lastname": "akwesi",
-        "email": "email"
-    }), function (err, result) {
-        assert.equal(err, null);
-        console.log(err)
-        console.log("inserted");
-        console.log(result)
-    }*/
+    console.log("program is running")
 
-    app.post('/api/newUser', (req, res) => {
+    app.get('/', (req, res) => {
+        res.send('Hello Bitch!')
+        //res.sendFile(path.join(__dirname, 'html/home.html'))
+        //console.log(__dirname)
+    });
+    app.get("/getUser/:id", (req, res) => {
+        let userid = req.params.id
+        //console.log(parseInt(userid)
+        collection.find({
+            userId: parseInt(userid)
+        }).toArray(function (err, docs) {
+            //console.log(docs)
+            res.send(docs);
+        });
+    });
+
+    app.put('/UpdateData/:userid', (req, res) => {
+        //console.log(req.params.userid)
+        let userid = req.params.userid
+        //console.log('get update data')
+        //console.log(req.body.name)
+        collection.updateOne({
+            userId: parseInt(userid)
+        }, {
+            $set: {
+                "username": req.body.username,
+                "name": req.body.name,
+                "lastname": req.body.lastname,
+                "email": req.body.email
+            },
+        }, function (err, result) {
+            //console.log(err)
+            res.send(result)
+        })
+    })
+
+
+    app.post("/postNewUser", (req, res) => {
         if (req.body.password != req.body.passwordCheck) {
             console.log("password is not correct")
         } else {
@@ -55,53 +92,109 @@ let runProgram = () => {
             let password = req.body.password;
             let email = req.body.email;
 
+            bcrypt.hash(password, saltRounds, function (err, hash) {
                 collection.insertOne({
                     "userId": Math.round(Math.random() * 100),
                     "username": username,
                     "name": name,
                     "lastname": lastname,
-                    "password": password,
+                    "password": hash,
+                    //"password": password,
                     "email": email,
-                    "savedObjects": [],
-                    "ownTeam": []
+                    "dagboek": []
                 }, function (err, result) {
                     assert.equal(err, null);
                     console.log(err)
                     console.log("inserted");
-                    console.log(result)
+                    //console.log(result)
                 })
+            });
         }
-    })
+    });
+    app.post('/dagboek/:userid', (req, res) => {
+        let id = req.params.id
+        let verhaal = req.body.verhaal
+        let titel = req.body.titel
+        let userid = req.params.userid
+        collection.findOneAndUpdate({
+                userId: parseInt(userid)
+            }, {
+                $push: {
+                    "dagboek": {
+                        "verhaalId": Math.round(Math.random() * 100),
+                        "titel": titel,
+                        "verhaal": verhaal
+                    }
+                }
+            },
+            function (error, success) {
+                console.log("succes")
+            });
+    });
 
-    app.post('/api/video', (req, res) => {
-            let username = req.body.username;
-            let videoId = req.body.videoId;
-            let comment = req.body.comment
+    app.get("/userDagboek/:id", (req, res) => {
+        let userid = req.params.id
+        //console.log(parseInt(userid)
+        collection.find({
+            userId: parseInt(userid)
+        }).toArray(function (err, docs) {
+            //console.log(docs)
+            res.send(docs);
+        });
+    });
 
-            collection.insertOne({
-                "commentId": Math.round(Math.random() * 100),
-                "videoId": [{
-                    "videoId": videoId,
-                    "username": username,
-                    "comment": comment
-                }]
-            }, function (err, result) {
-                assert.equal(err, null);
-                console.log(err)
-                console.log("inserted");
-                console.log(result)
-            })
-    })
-
-    app.post('/api/login', function (req, data) {
+    app.post('/login', function (req, data) {
+        //console.log("login triggerds")
         let username = req.body.username;
         let passwordLogin = req.body.password;
-        console.log(passwordLogin)
+        //console.log(passwordLogin)
         let mongoPass;
-        console.log(mongoPass)
+        //console.log(mongoPass)
 
-        //})
+        collection.find({
+            "username": username
+        }).toArray(function (err, result) {
+            result.forEach(function (item) {
+                mongoPass = item.password;
+            })
+            bcrypt.compare(passwordLogin, mongoPass, function (err, res) {
+                //console.log(mongoPass)
+                //console.log(res)
+                if (res === true) {
+                    //console.log("logged in")
+                    data.send(result)
+                    //data.send(true)
+                } else {
+                    //console.log("wrong password")
+                    data.send(false)
+                }
+            })
+        })
     })
+
+
+    app.post('/video', (req, res) => {
+        let username = req.body.username;
+        let videoId = req.body.videoId;
+        let comment = req.body.comment
+        collection.findOneAndUpdate({
+                videoId: parseInt(videoId)
+            }, {
+                $push: {
+                    "userId": userId,
+                    "commentId": Math.round(Math.random() * 100),
+                    "videoId": [{
+                        "videoId": videoId,
+                        "username": username,
+                        "comment": comment
+                    }]
+                }
+            },
+            function (error, success) {
+                console.log("succes")
+            });
+    })
+
 
     app.get('/api/getUserData/:id', (req, res) => {
         let userid = req.params.id
@@ -113,11 +206,6 @@ let runProgram = () => {
             res.send(docs);
         });
     })
-
-
-    app.get('/', (req, res) => {
-        res.send('Hello Bitch!')
-    });
 };
 
 
@@ -125,4 +213,3 @@ let runProgram = () => {
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
 });
-
--- js/home.js
+++ js/home.js
@@ -5,10 +5,15 @@ $(document).ready(function () {
     newUser();
     stroornissen();
     zoekHulp();
+    profiel();
+    dagboek();
+    //dagboekShow();
 
     $('.stoornisDetail').hide();
     $('.zoekHulpDetail').hide();
-
+    $('.profielInfo').hide();
+    $('.dagboek').hide();
+    $('.updateProfile').hide();
 
     $('.leesMeer').on('click', function () {
         $('.stoornis').hide();
@@ -30,8 +35,112 @@ function quote() {
     })
 }
 
+function profiel() {
+    let userid = localStorage.getItem("userId")
+    //console.log(userid)
+    $.ajax({
+        url: `http://127.0.0.1:63342/getUser/${userid}`,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (data) {
+        //console.log(data)
+        let userData = data
+        userData = userData.sort();
+        console.log(data)
+        for (let i = 0; i < userData.length; i += 1) {
+            let result = userData[i];
+            let storeID = result.userId;
+
+            //console.log(result.userId)
+            $("#navLogin").empty();
+            $("#navLogin").text("profiel");
+            $(".loginForm").hide()
+            $('.profielInfo').show();
+
+            let div = $(`<div class='userProfiel' id= ${result.id}>`);
+
+            $(`<p type=text id= ${result.userId}>`).text(`Username: ${result.username}`).appendTo(div)
+            $(`<p type=text id= ${result.userId}>`).text(`Naam: ${result.name}`).appendTo(div);
+            $(`<p type=text id= ${result.userId}>`).text(`Achternaam: ${result.lastname}`).appendTo(div);
+            $(`<p type=text id= ${result.userId}>`).text(`Email: ${result.email}`).appendTo(div);
+            $(`<button type="button" class="bewerk" id= ${result.userId}>`).text('update').appendTo(div);
+            $(`<button type="button" class="logOut" id= ${result.userId}>`).text('logOut').appendTo(div);
+            $(`<button type="button" class="dagboekButton" id= ${result.userId}>`).text('Dagboek').appendTo(div);
+
+            $(".profielInfo").append(div);
+        }
+
+        $(".dagboekButton").on("click", function () {
+            $('.dagboek').show();
+            dagboekShow(data)
+            //dagboekShow();
+        })
+
+        $(".bewerk").on("click", function () {
+            $('.dagboek').hide();
+            $('.profielInfo').hide();
+            $('.updateProfile').show();
+            updateData()
+        })
+
+        $(".logOut").on("click", function () {
+            logOut()
+        })
+
+    })
+}
+
+function logOut() {
+    let userid = localStorage.getItem("userId")
+    $.ajax({
+        url: `http://localhost:63342/getUser/${userid}`,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (data) {
+        console.log(data);
+        window.localStorage.clear();
+        window.location.href = "home.html";
+    })
+}
+
+function dagboek() {
+    let userid = localStorage.getItem("userId")
+    $('.dagboekVerstuur').on("click", function (e) {
+        e.preventDefault()
+        let verhaal = {
+            titel: $('.dagboekTitel').val(),
+            verhaal: $('.dagboekVerhaal').val(),
+        }
+        console.log(verhaal);
+        $.ajax({
+            url: `http://localhost:63342/dagboek/${userid}`,
+            type: 'POST',
+            data: verhaal,
+        }).done(function (data) {
+            console.log(data)
+        })
+    })
+}
+
+function dagboekShow(userData) {
+    let userid = localStorage.getItem("userId")
+    //let result = userData[0].dagboek;
+    let data = userData[0].dagboek
+    for (let dag of data) {
+        console.log(dag)
+        //let dag = result[i]
+        //console.log(dag)
+        let div = $(`<div class='verhaal' id= ${dag.verhaalId}>`);
+
+        $(`<p type=text id= ${dag.verhaalId}>`).text(`Titel: ${dag.titel}`).appendTo(div)
+        $(`<p type=text id= ${dag.verhaalId}>`).text(`verhaal: ${dag.verhaal}`).appendTo(div);
+
+        $(".dagboekContent").append(div);
+    }
+}
+
 function login() {
-    $('.loginFormulier').hide();
+    /*$('.loginFormulier').hide();
     $('.login').on('click', function () {
         $('.loginFormulier').show();
         $('.profielInfo').hide();
@@ -39,29 +148,32 @@ function login() {
         $('.dagboekbutton').hide();
     });
 
-    $('.updateProfile').hide()
+    $('.updateProfile').hide()*/
     $("form.login").submit(function (e) {
         let user = {
             username: $('#username').val(),
             password: $('#password').val(),
         }
-        $('#username').val('')
-        $('#password').val('')
+        //$('#username').val('')
+        //$('#password').val('')
         e.preventDefault();
         console.log("pressed");
-        console.log(user)
+        //console.log(user)
         $.ajax({
-            url: "http://127.0.0.1:63342/api/login",
+            url: "http://localhost:63342/login",
             type: "POST",
             data: user,
             success: function (data) {
                 console.log('added!')
-                console.log(data)
+                let userId = data[0].userId
+                //profiel(data)
 
                 if (data == false) {
                     console.log("je bent fout")
                 } else {
                     window.location.href = "home.html";
+                    localStorage.setItem("userId", userId);
+
                 }
             }
         }).fail(function (err1, err2) {
@@ -71,9 +183,55 @@ function login() {
     });
 }
 
+function updateData() {
+    let userid = localStorage.getItem("userId")
+    $.ajax({
+        url: `http://localhost:63342/getUser/${userid}`,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (data) {
+        console.log(data);
+        for (let user of data) {
+            $("#updateUsername").val(user.username);
+            $("#updateName").val(user.name);
+            $("#updateLastName").val(user.lastname);
+            $("#updateEmail").val(user.email);
+        }
+
+        $('form#updateProfileForm').submit(function (e) {
+            e.preventDefault();
+
+            let userupdate = {
+                username: $("#updateUsername").val(),
+                name: $("#updateName").val(),
+                lastname: $("#updateLastName").val(),
+                email: $("#updateEmail").val()
+            }
+
+            console.log("pressed");
+            $.ajax({
+                url: `http://localhost:63342/UpdateData/${userid}`,
+                type: "PUT",
+                data: userupdate,
+                success: function (data) {
+                    //window.location.href = "profile.html";
+                    console.log('updated!')
+                    console.log(data)
+
+                }
+            }).fail(function (err1, err2) {
+                console.log(err1)
+                console.log(err2)
+            })
+        })
+    })
+}
+
 
 function newUser() {
-    $('form#toevoegenForm').submit(function () {
+    console.log("newUser")
+    $('form#toevoegenForm').submit(function (e) {
+        e.preventDefault()
         let newUser = {
             username: $('#newUsername').val(),
             name: $('#newName').val(),
@@ -85,10 +243,12 @@ function newUser() {
         console.log(newUser);
         console.log('pressed');
         $.ajax({
-            url: 'http://127.0.0.1:63342/api/newUser',
+            url: "http://localhost:63342/postNewUser",
             type: 'POST',
             data: newUser,
-            success: function (data) {
+            success: function (data, e) {
+                e.preventDefault()
+                console.log(data)
                 console.log('added!')
                 //window.location.href = 'login.html';
             }
@@ -180,7 +340,7 @@ function zoekHulp() {
         type: 'GET',
         dataType: 'json',
     }).done(function (data) {
-        console.log(data);
+        //console.log(data);
         for (let result of data.data) {
             //console.log((result))
             let div = $(`<div class='hulpDiv' id= ${result.id}>`);
